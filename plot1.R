264d77006a91227795542c8dda08b2b87078342f
dmsimler <dmsimler@hotmail.com>
2014-12-07 08:49:54 -0800

    Converted the plot in plot1.R to a function

--- plot1.R
+++ plot1.R
@@ -1,11 +1,21 @@
+## ################################################################################################
+## Function Plot1 - Creates the 1st plot for Course Assignment 1.  
+##                  The plot is a histogram of the Global_active_power field that is saved to a png file. 
+##
+## Notes - Made this a function for maintainability and understandibility, but the assignment could
+##         have also just been a straight R script.
+##         Takes as an input, the data file and plot file names
+## ################################################################################################
+plot1 <- function(data_file_name, plot_file_name) {
+  
 ## First read in the data from the file
-power_consumption_ds <- read.table("household_power_consumption.txt", header=TRUE, sep=";", colClasses = c("character", "character", rep("numeric", 1, 7)), na.strings="?")
+power_consumption_ds <- read.table(data_file_name, header=TRUE, sep=";", colClasses = c("character", "character", rep("numeric", 1, 7)), na.strings="?")
 
 ## Now select out a subset of the original data between 2007-02-01 and 2007-02-02
 power_consumption_ds <- power_consumption_ds[with(power_consumption_ds, Date == "1/2/2007" | Date == "2/2/2007"), ]
 
 ## Open up the png device to which the histogram will be written
-png(filename = "plot1.png", width = 480, height = 480, units = "px")
+png(filename = plot_file_name, width = 480, height = 480, units = "px")
 
 ## Now create a histogram of the Global_active_power data with the correct title, x-axis label, and color
 ## for the bars.  Settling on orangered2 as the bar color since it most closely matches
@@ -14,3 +24,4 @@ hist(power_consumption_ds$Global_active_power, col = "orangered2", main = "Globa
 
 ## Now close the png device
 dev.off()
+}
